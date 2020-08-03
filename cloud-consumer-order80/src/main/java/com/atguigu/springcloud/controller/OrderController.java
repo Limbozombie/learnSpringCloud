a10cff3d33e77ed4815c9e06a49b0acf9f2471d1
limbo <271636872@qq.com>
2020-08-03 22:34:39 +0800

    Robbin

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -2,7 +2,10 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,16 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class OrderController {
 
-//	    public static final String PAYMENT_URL = "http://localhost:8001";
+	//	    public static final String PAYMENT_URL = "http://localhost:8001";
 	public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
 	@Resource
 	private RestTemplate restTemplate;
+
+	@Resource
+	private LoadBalancer loadBalancer;
+
+	@Resource
+	private DiscoveryClient discoveryClient;
 
 	@GetMapping("/consumer/payment/create")
 	public CommonResult<Payment> create(@RequestBody Payment payment) {
@@ -38,4 +49,17 @@ public class OrderController {
 		}
 	}
 
+	@GetMapping("/consumer/payment/lb")
+	public String getPaymentLB() {
+		// 通过容器中的 discoveryClient和服务名来获取服务集群
+		List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+		if (instances == null || instances.size() <= 0) {
+			return null;
+		}
+		// 传入服务集群来计算出获取具体的服务实例
+		ServiceInstance serviceInstance = loadBalancer.instances(instances);
+		URI uri = serviceInstance.getUri();
+		String url = uri + "/payment/lb";
+		return restTemplate.getForObject(url, String.class);
+	}
 }
